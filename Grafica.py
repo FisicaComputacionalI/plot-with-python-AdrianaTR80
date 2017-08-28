72cb4aeab433510b4e7a0784bb0694ac91fb54aa
AdrianaTR80 <31289704+AdrianaTR80@users.noreply.github.com>
2017-08-28 13:16:54 -0500

    Add files via upload
    Tarea

--- Grafica.py
+++ Grafica.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-plt.plot( [4,0,0,0,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0])
+plt.plot([4,0,0,0,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0])
 plt.ylabel('Hermanos y primos de acuerdo a la edad[años]')
 plt.savefig('temp.jpeg')
 plt.show()
