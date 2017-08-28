3709cf7cff3b7525eca2b5e5b5b8c8d277e7a1bc
AdrianaTR80 <31289704+AdrianaTR80@users.noreply.github.com>
2017-08-28 12:47:41 -0500

    Tarea
    Ejercicio terminado

---
+++ Grafica.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19], [4,0,0,0,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0], 'ro')
+plt.ylabel('Hermanos y primos de acuerdo a la edad[años]')
+plt.savefig('temp.jpeg')
+plt.show()
+
