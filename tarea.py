3aa77b692441bb3822a7aeac2fee5f5e65aaa4a8
alumno <alumno@mod126.fcfm.buap.mx>
2017-08-28 09:03:36 -0500

    Ejercicio

---
+++ tarea.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([4,0,0,0,1,1,1,1,0,0,0,0,0,0,0,1,0,0,0,0])
+plt.ylabel('Hermanos y primos de acuerdo a la edad[años]')
+plt.savefig('temp.jpeg')
+plt.show()
+
